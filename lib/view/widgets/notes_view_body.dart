edd87fc0ac3f909f74e45a74d63da0549bb2984f
MostafaMNaguib <mostafa.mohamad.naguib@gmail.com>
2023-07-27 16:37:15 +0300

    create project and initiate views

---
+++ lib/view/widgets/notes_view_body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+
+
+
+}
