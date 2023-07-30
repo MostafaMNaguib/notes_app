a62dd9e3603b6c9a5fb01cb48bd6b0e9044d6751
MostafaMNaguib <mostafa.mohamad.naguib@gmail.com>
2023-07-30 11:23:57 +0300

    creating note item

---
+++ lib/view/widgets/custom_app_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/view/widgets/custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: const [
+        Text(
+            'Notes',
+          style: TextStyle(
+            fontSize: 28
+          ),
+        ),
+        CustomSearchIcon()
+      ],
+    );
+  }
+}
+
