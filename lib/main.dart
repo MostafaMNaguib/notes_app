35d555896739b4831e67bee8145d8be74f71c765
MostafaMNaguib <mostafa.mohamad.naguib@gmail.com>
2023-07-30 15:57:29 +0300

    initializing hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/utils/constants.dart';
 import 'package:notes_app/view/notes_view.dart';
 
-void main() {
+void main()async {
+
+  await Hive.initFlutter();
+  await Hive.openBox(noteBox);
+
   runApp(const MyApp());
 }
 
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,5 @@
 import 'dart:ui';
 
 const primaryColor = Color(0xff62FCD7);
+
+const String noteBox = 'notes_box';
